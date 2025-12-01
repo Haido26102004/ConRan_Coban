50c5c799adc8c296087e7d7f035466c19cb4918d
Hai <haido26102004@gmail.com>
2025-12-01 14:06:44 +0700

    update2

---
+++ 12-1-2025/func.py
@@ -0,0 +1,5 @@
+
+def hcn(cd,cr):
+    S = (cd+cr)*2
+    CV = cd*cr
+    return S,CV
--- 12-1-2025/kt.py
+++ 12-1-2025/kt.py
@@ -1,11 +1,8 @@
+import func
 try:
     cd = int(input("chieu dai la :"))
     cr = int(input("chieu rong la :"))
 except ValueError:print("Loi nhap sai kieu du lieu")
 else:
-    def hcn(cd,cr):
-        S = (cd+cr)*2
-        CV = cd*cr
-        return S,CV
-    tuple = hcn(cd,cr)
+    tuple = func.hcn(cd,cr)
     print (f"Dien tich hinh chu nhat la {tuple[0]} va Chu vi hinh chu nhat la {tuple[1]}")
