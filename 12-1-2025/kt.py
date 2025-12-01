5ec25ea2444046b9ac0df5a56cbf6e997bd82df5
Hai <haido26102004@gmail.com>
2025-12-01 14:51:34 +0700

    update2

--- 12-1-2025/kt.py
+++ 12-1-2025/kt.py
@@ -3,7 +3,8 @@ while True:
     try:
         a = int(input("a la :"))
         b = int(input("b la :"))
+        if a>0 and b>0:
+            break
     except ValueError:
         print("Loi nhap sai kieu du lieu")
-    else:
-        print(f"UCBC cua {a} va {b} la {func.ucbc(a,b)}")
+print(f"UCBC cua {a} va {b} la {func.ucbc(a,b)}")
